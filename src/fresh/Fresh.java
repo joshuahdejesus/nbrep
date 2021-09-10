71a70b63b09ee9cdcc3697cd8d527c339622f723
joshuahdejesus <joshuahdejesus@yahoo.com>
2021-09-10 13:53:35 -0700

    Update Fresh.java

--- src/fresh/Fresh.java
+++ src/fresh/Fresh.java
@@ -5,7 +5,9 @@ public class Fresh {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        
+        System.out.println("Hey from GitHub!");       
+        
      
     }
-
 }
